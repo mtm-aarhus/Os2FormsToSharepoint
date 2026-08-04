1410cafabdbe1a1c8baced282dbf7d8cf2164803
Laura-Salmonsen <balas@aarhus.dk>
2026-08-04 08:45:28 +0200

    titel

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -64,6 +64,7 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
 
     queue_data = json.loads(queue_element.data)
     anmodnings_id = queue_data.get("application_uuid")
+    formular_titel = queue_data.get("formular")
 
     # --- Credentials / SharePoint-kontekst ---
     os2forms_user = orchestrator_connection.get_credential("OS2FormsAPI")
@@ -72,7 +73,8 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
 
     certification = orchestrator_connection.get_credential("SharePointCert")
     api = orchestrator_connection.get_credential("SharePointAPI")
-    base_url = f'{orchestrator_connection.get_constant("AarhusKommuneSharePoint").value}/Teams/tea-teamsite12592'
+    base_url = f'{orchestrator_connection.get_constant("AarhusKommuneSharePoint").value}'
+    altinget_endelse = '/Teams/tea-teamsite12592'
 
     cert_credentials = {
         "tenant": api.username,
@@ -80,48 +82,49 @@ def process(orchestrator_connection: OrchestratorConnection, queue_element: Queu
         "thumbprint": certification.username,
         "cert_path": certification.password,
     }
-    ctx = ClientContext(base_url).with_client_certificate(**cert_credentials)
-    ctx.load(ctx.web)
-    ctx.execute_query()
+    if  formular_titel.lower() == "Tilmelding til Altinget".lower():
+        ctx = ClientContext(f'{base_url}{altinget_endelse}').with_client_certificate(**cert_credentials)
+        ctx.load(ctx.web)
+        ctx.execute_query()
 
-    # --- Hent submission fra OS2Forms ---
-    url = f"{os2forms_url}tilmelding_til_altinget/submission/{anmodnings_id}"
-    headers = {"api-key": os2forms_api_key}
-    response = requests.get(url, headers=headers, timeout=60)
-    response.raise_for_status()
+        # --- Hent submission fra OS2Forms ---
+        url = f"{os2forms_url}tilmelding_til_altinget/submission/{anmodnings_id}"
+        headers = {"api-key": os2forms_api_key}
+        response = requests.get(url, headers=headers, timeout=60)
+        response.raise_for_status()
 
-    payload = response.json()
-    # "data" ligger på top-niveau ved siden af "entity" (ikke inde i entity)
-    form = payload["data"]["mine_medarbejder_data"]
-    if not form.get("magistrat"):
-        az_ident = form.get("az")
-        magistrat = get_magistrat_from_fdw(orchestrator_connection, az_ident)
-        if magistrat != "Teknik og Miljø":
-            send_forkert_mag_mail(az_ident, orchestrator_connection= orchestrator_connection)
-        if magistrat:
-            form["magistrat"] = magistrat
-            orchestrator_connection.log_info(f"Magistrat var tom, hentet fra FDW: {magistrat}")
-        else:
-            orchestrator_connection.log_info("Magistrat var tom, og kunne ikke findes i FDW.")
-    submission_uuid = payload["entity"]["uuid"][0]["value"]  # global unik – til evt. dedup
+        payload = response.json()
+        # "data" ligger på top-niveau ved siden af "entity" (ikke inde i entity)
+        form = payload["data"]["mine_medarbejder_data"]
+        if not form.get("magistrat"):
+            az_ident = form.get("az")
+            magistrat = get_magistrat_from_fdw(orchestrator_connection, az_ident)
+            if magistrat != "Teknik og Miljø":
+                send_forkert_mag_mail(az_ident, orchestrator_connection= orchestrator_connection)
+            if magistrat:
+                form["magistrat"] = magistrat
+                orchestrator_connection.log_info(f"Magistrat var tom, hentet fra FDW: {magistrat}")
+            else:
+                orchestrator_connection.log_info("Magistrat var tom, og kunne ikke findes i FDW.")
+        submission_uuid = payload["entity"]["uuid"][0]["value"]  # global unik – til evt. dedup
 
-    # Venstre side = de PRÆCISE nøgler fra form-JSON'en.
-    # Højre side  = de interne SharePoint-kolonnenavne.
-    column_mapping = {
-        "name":                  "Title",
-        "az":                    "Az_x002d_ident",
-        "organisation_enhed":    "Afdeling",                          
-        "organisation_niveau_2": "Organisatoriskenhedovermedarbejd",  
-        "magistrat":             "Magistratsafdeling1",                
-    }
+        # Venstre side = de PRÆCISE nøgler fra form-JSON'en.
+        # Højre side  = de interne SharePoint-kolonnenavne.
+        column_mapping = {
+            "name":                  "Title",
+            "az":                    "Az_x002d_ident",
+            "organisation_enhed":    "Afdeling",                          
+            "organisation_niveau_2": "Organisatoriskenhedovermedarbejd",  
+            "magistrat":             "Magistratsafdeling1",                
+        }
 
-    add_item_to_sharepoint_list(
-        orchestrator_connection=orchestrator_connection,
-        client=ctx,
-        list_title="Tilmeldte medarbejdere",
-        data=form,
-        column_mapping=column_mapping,
-    )
+        add_item_to_sharepoint_list(
+            orchestrator_connection=orchestrator_connection,
+            client=ctx,
+            list_title="Tilmeldte medarbejdere",
+            data=form,
+            column_mapping=column_mapping,
+        )
 
 
 def get_internal_column_names(client: ClientContext, list_title: str) -> dict:
